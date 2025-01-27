84ce3dc093611a7d5ae46e2444b71e1ae318b825
Forest Agostinelli <foresta@cse.sc.edu>
2025-01-26 22:56:25 -0500

    run_hw0 fix

--- run_hw0.py
+++ run_hw0.py
@@ -11,7 +11,7 @@ def main():
     args = parser.parse_args()
 
     grid = load_grid(args.map)
-    env: FarmGridWorld = FarmGridWorld(grid.shape, 0.0)
+    env: FarmGridWorld = FarmGridWorld("aifarm", grid.shape, 0.0)
 
     viz: InteractiveFarm = InteractiveFarm(env, grid)
 
